166b6bafbcba03b8705d230fa2237e9900ed0afd
Aman8167 <118344734+Aman8167@users.noreply.github.com>
2023-10-08 19:27:03 +0530

    Add files via upload

--- Payment2.js
+++ Payment2.js
@@ -10,60 +10,61 @@ function Payment2() {
 <div className="col-md-10">
 <div className="card-body mt-5">
 <form>
-<div className="form-group">
-<label> Company Name:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Company Name
- "
-/>
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Company Name</label>
+      <input type="text" class="form-control" placeholder="Enter your Company name" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Duty Slip No</label>
+      <input type="text" class="form-control"  placeholder="Enter your Duty slip" />
+      
+    </div>
+  </div>
 </div>
-<div className="form-group">
-<label> Duty Slip No:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Duty Slip No
- " 
-/>
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Reporting Address</label>
+      <input type="text" class="form-control" placeholder="Enter your Reporting Address" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Date</label>
+      <input type="date" class="form-control"  placeholder="Enter your Date" />
+      
+    </div>
+  </div>
 </div>
-<div className="form-group">
-<label> Reporting Address:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Reporting Address
- " 
-/>
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Name</label>
+      <input type="text" class="form-control" placeholder="Enter your Name" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Vehicle Number</label>
+      <input type="number" class="form-control"  placeholder="Enter your Vehicle Number" />
+      
+    </div>
+  </div>
 </div>
-<div className="form-group">
-<label> Date :</label>
-<input 
-  type="Date"
-className="form-control  "
-placeholder="Enter  your Date 
- " 
-/>
-</div>
-<div className="form-group">
-<label>  Name :</label>
-<input 
-  type="time"
-className="form-control  "
-placeholder="Enter  your Name
- " 
-/>
-</div>
-<div className="form-group">
-<label> Vehicle Number:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Vehicle Number
- " 
-/>
-</div>
+
+
+
+
+
 <div className="form-group">
 <label> Vehicle Type :</label>
 <select  class="form-control  "id="SelectOption">
@@ -73,171 +74,185 @@ placeholder="Enter  your Vehicle Number
             </select>
 
 </div>
-<div className="form-group">
-<label> From :</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your From
- " 
-/>
-</div>
-<div className="form-group">
-<label> To :</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your To
- " 
-/>
-</div>
 
-<div className="form-group">
-<label> Closing Kms:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Closing Kms
- " 
-/>
-</div>
-<div className="form-group">
-<label> Closing Time:</label>
-<input 
-  type="Time"
-className="form-control  "
-placeholder="Enter  your Closing Time
- " 
-/>
-</div>
-<div className="form-group">
-<label> Starting Kms:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Starting Kms
- " 
-/>
-</div>
-<div className="form-group">
-<label> Reporting Kms:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Reporting Kms
- " 
-/>
-</div>
-<div className="form-group">
-<label> Total Kms:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Total Kms
- " 
-/>
-</div>
-<div className="form-group">
-<label> Total Hours:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Total Hours
- " 
-/>
-</div>
-<div className="form-group">
-<label> 8 Hrs.  80Kms.@:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your 8 Hrs.  80Kms.@
- " 
-/>
-</div>
-<div className="form-group">
-<label> Amount:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Amount
- " 
-/>
-</div>
-<div className="form-group">
-<label> Extra Kms:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Extra Kms
- " 
-/>
-</div>
-<div className="form-group">
-<label> Amount:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Amount
- " 
-/>
-</div>
-<div className="form-group">
-<label> Extra Hours:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Extra Hours
- " 
-/>
-</div>
-<div className="form-group">
-<label>Amount:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Amount
- " 
-/>
-</div>
-<div className="form-group">
-<label>1 Day 300 kms:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your 1 Day 300 Kms
- " 
-/>
-</div>
-<div className="form-group">
-<label>Amount:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Amount
- " 
-/>
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >From</label>
+      <input type="text" class="form-control" placeholder="Enter your From" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >To</label>
+      <input type="text" class="form-control"  placeholder="Enter your To" />
+      
+    </div>
+  </div>
 </div>
 
 
-<div className="form-group">
-<label>Total Amount:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Total Amount
- " 
-/>
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Closing Kms</label>
+      <input type="text" class="form-control" placeholder="Enter your Closing Kms" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Closing Time</label>
+      <input type="Time" class="form-control"  placeholder="Enter your Closing Time" />
+      
+    </div>
+  </div>
 </div>
-<div className="form-group">
-<label>Advance Payment:</label>
-<input 
-  type="text"
-className="form-control  "
-placeholder="Enter  your Advance Payment
- " 
-/>
+
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Starting Kms</label>
+      <input type="text" class="form-control" placeholder="Enter your Starting Kms" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Reporting Kms</label>
+      <input type="Text" class="form-control"  placeholder="Enter your Reporting Kms" />
+      
+    </div>
+  </div>
 </div>
+
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Total Kms</label>
+      <input type="text" class="form-control" placeholder="Enter your Total Kms" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Total Hours</label>
+      <input type="Text" class="form-control"  placeholder="Enter your Total Hours" />
+      
+    </div>
+  </div>
+</div>
+
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >8 Hrs.  80Kms.@</label>
+      <input type="text" class="form-control" placeholder="Enter your 8 Hrs.  80Kms.@" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Amount</label>
+      <input type="number" class="form-control"  placeholder="Enter your Amount" />
+      
+    </div>
+  </div>
+</div>
+
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Extra Kms</label>
+      <input type="text" class="form-control" placeholder="Enter your Extra Kms" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Amount</label>
+      <input type="number" class="form-control"  placeholder="Enter your Amount" />
+      
+    </div>
+  </div>
+</div>
+
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Extra Hours</label>
+      <input type="text" class="form-control" placeholder="Enter your Extra Kms" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Amount</label>
+      <input type="number" class="form-control"  placeholder="Enter your Amount" />
+      
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >1 Day 300 kms</label>
+      <input type="text" class="form-control" placeholder="Enter your 1 Day 300 kms" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Amount</label>
+      <input type="number" class="form-control"  placeholder="Enter your Amount" />
+      
+    </div>
+  </div>
+</div>
+
+
+
+<div class="row g-2">
+  <div class="col-md">
+    <div class="form-group">
+    <label >Total Amount</label>
+      <input type="text" class="form-control" placeholder="Enter your Total Amount" />
+      
+    </div>
+  </div>
+  <div class="col-md">
+    <div class="form-group">
+    <label >Advance Payment</label>
+      <input type="number" class="form-control"  placeholder="Enter your Advance Payment" />
+      
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <br></br>
